d766fa5421680a42801a2bf3366cd4c068a0ab1c
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-04-07 16:25:19 -0400

    updated project urls + created first view function + created songs app urls file

--- songs/views.py
+++ songs/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
 
 @api_view(["GET"])
 def songs_list(request):
+    songs = Song.objects.all()
+    
+    serializer = SongSerializer(songs, many=True)
     
     
-    return Response('ok')
+    return Response(serializer.data)
     
     
